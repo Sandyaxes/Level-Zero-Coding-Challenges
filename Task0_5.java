4bfe9a9fa8ce08ef65285337bca6cb20b6794f95
Sandyaxes <45788837+Sandyaxes@users.noreply.github.com>
2021-01-25 14:01:15 -0800

    Update Task0_5.java

--- Task0_5.java
+++ Task0_5.java
@@ -1,15 +1,18 @@
+import static java.lang.Math.sqrt;
+
 public class Task0_5 {
     
-       static int TriangleArea(int num1, int num2, int num3, String...args){
+       static int TriangleArea(int num1, int num2, int num3){
             
-            int area = (num1 + num2 + num3) / 2;
+            int semiPerimeter = (num1 + num2 + num3) / 2;
+            int area = (int)sqrt(semiPerimeter * (semiPerimeter - num1)*(semiPerimeter - num2)*(semiPerimeter - num3));
             return area;
         
     }
     
     public static void main(String args[]) {
         
-       System.out.println("Triangle area is " + TriangleArea(5,7,8) + "cm");
+       System.out.println("Triangle area is " + TriangleArea(4,5,6) + " sq.units");
 
 
     }
