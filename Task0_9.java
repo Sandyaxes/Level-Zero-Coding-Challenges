4451f22c9cfcb2071e5fa3e0866b65f5d4f17de9
Sandyaxes <45788837+Sandyaxes@users.noreply.github.com>
2021-01-27 00:38:11 -0800

    Update Task0_9.java

--- Task0_9.java
+++ Task0_9.java
@@ -5,7 +5,7 @@ public class Task0_9 {
         char[] nameArr = new char[name.length()];
 
         for(int i = 0; i < name.length(); i++){
-            char ch = name.charAt(i);    // Using charAt method to return specific index from a string name;
+            char ch = name.charAt(i);    
 
             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
                 nameArr[i] = ch;
